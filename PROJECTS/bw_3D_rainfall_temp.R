3aaf6c36f27e5fab73237e0e6a5e77955ea7a5b6
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2025-04-14 12:14:29 +0200

    add packages

--- PROJECTS/bw_3D_rainfall_temp.R
+++ PROJECTS/bw_3D_rainfall_temp.R
@@ -1,1 +1,17 @@
 getwd()
+# 1. PACKAGES
+#------------------
+install.packages("remotes")
+remotes::install_github(
+  "inSileco/rchelsa"
+)
+remotes::install_github(
+  "chris-prener/biscale"
+)
+
+# install and load all packages
+pacman::p_load(
+  geodata, tidyverse, sf, terra,
+  rchelsa, biscale, elevatr, cowplot,
+  gridGraphics, rayshader
+)
